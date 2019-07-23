bb2c07a5a0f681e4812118757f740d14ddf96e89
Guilherme Moura <guilhermemouraa5@gmail.com>
2019-07-23 17:23:10 -0300

    adicinar funçao

---
+++ script.js
@@ -0,0 +1,67 @@
+// let compras = ['salgadinhos', 'coca-cola', 'chocolate', 'alcaxofra', 'pepino'];
+// console.log(compras);
+// //ADICIONAR NA ULTIMA POSIÇÃO
+// compras.push("cebola");
+// console.log(compras);
+// //REMOVER O ULTIMO ELEMENTO
+// compras.pop("pepino");
+// console.log(compras);
+// //ADICINAR ELEMENTO NA PRIMEIRA POSIÇÃO
+// compras.unshift("macarrão");
+// console.log(compras);
+// //REMOVER O PRIMEIRO ELEMENTO
+// compras.shift();
+// console.log(compras);
+
+
+
+
+// function somar(num1, num2){
+//     return (num1 + num2);   
+// }
+
+// function subtrair(num1, num2){
+//     console.log(num1 - num2);
+// }
+
+// alert(somar(10, 184));
+
+// let casa = [{nome: 'lucas'},
+//             {sobrenome:'melo'}]
+
+
+    
+// }
+
+
+// let corpo = document.querySelector("body");
+// let botao = document.querySelector(".button");
+
+// function trocarCor(){
+//     corpo.style.backgroundColor = "limegreen";
+// }
+
+// botao.onclick = trocarCor;
+
+
+// let corpo = document.querySelector("body");
+// let botoes = document.querySelectorAll("button");
+// let imagem = document.querySelector("imagem")
+
+// function trocarCor(){
+//     corpo.style.backgroundColor = "limegreen";
+// }
+
+// function trocarTexto(){
+//     botoes[1].innerHTML = "sou o Novo texto";
+// }
+
+// function trocarFoto(){
+//     Image.src = "burguer.png"
+// }
+
+// botoes[0].onclick = trocarCor;
+// botoes[1].onclick = trocarTexto;
+// botoes[2].onclick = trocarFoto;
+
+
